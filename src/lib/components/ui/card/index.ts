65dcdd523d1d1239fcdbc573137a99f951838a4a
joshidoshi <joshua.dietiker@gmail.com>
2025-12-09 18:29:30 +0100

    Add test data for journey connections from Luzern to Aarau
    - Introduced a new test data file containing detailed journey information.
    - Included multiple connections with departure and arrival details, including timestamps, delays, and platform information.
    - Structured data to include sections for each journey, detailing stops and relevant station information.

---
+++ src/lib/components/ui/card/index.ts
@@ -0,0 +1,25 @@
+import Root from "./card.svelte";
+import Content from "./card-content.svelte";
+import Description from "./card-description.svelte";
+import Footer from "./card-footer.svelte";
+import Header from "./card-header.svelte";
+import Text from "./card-text.svelte";
+import Action from "./card-action.svelte";
+
+export {
+	Root,
+	Content,
+	Description,
+	Footer,
+	Header,
+	Text,
+	Action,
+	//
+	Root as Card,
+	Content as CardContent,
+	Description as CardDescription,
+	Footer as CardFooter,
+	Header as CardHeader,
+	Text as CardText,
+	Action as CardAction,
+};
--- src/routes/quick/journey/[from]/[to]/+page.svelte
+++ src/routes/quick/journey/[from]/[to]/+page.svelte
@@ -1,16 +1,70 @@
-<script lang="ts">
+<script>
 	import { page } from '$app/state';
+
+	import data from './testdata.js';
+
+	import * as Item from '$lib/components/ui/item/index.js';
+	import { Badge } from '$lib/components/ui/badge/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
+	import * as Card from '$lib/components/ui/card/index.js';
 
 	let origin = $state('');
 	let destination = $state('');
 
-	$effect(() => {
-		origin = page.params.from!;
-		destination = page.params.to!;
-	});
+	let requestURL = 'http://transport.opendata.ch/v1/connections';
+
+	let connections = $state();
+
+	/*$effect(
+		async
+		() => {
+			origin = page.params.from ?? '';
+			destination = page.params.to ?? '';
+
+			let urlWithParams = `${requestURL}?from=${origin}&to=${destination}&limit=2`;
+			let response = await fetch(urlWithParams);
+			let data = await response.json();
+		}
+	);*/
+
+	connections = data?.connections ?? [];
 </script>
 
 <div class="flex h-screen flex-1 flex-col justify-end gap-3 pb-14">
-	<h1 class="text-3xl">From: {origin}</h1>
-	<h1 class="text-3xl">To: {destination}</h1>
+	{#each connections as connection}
+		{@const changeCount = (connection[0]?.products.length ?? 1) - 1}
+		{@const initialTrain = connection?.products[0] ?? 'Walk'}
+		{@const initialTrainDirection = connection.sections[0].journey.to}
+		{@const initalPlatform = connection?.from.platform}
+
+		<Card.Root>
+			<Card.Header>
+				<Card.Text>
+					<div class="flex items-center gap-2">
+						<Badge variant="destructive">{initialTrain}</Badge>
+						<span>
+							Direction <span class="font-bold">{initialTrainDirection}</span>
+						</span>
+					</div>
+
+					<span class="font-bold">Pl. {initalPlatform}</span>
+				</Card.Text>
+			</Card.Header>
+
+			<Card.Content>
+				<Separator class="flex-1" />
+				Change {changeCount} times
+				<Separator class="flex-1" />
+
+				{connection.from.departureTimestamp} - {connection.to.arrivalTimestamp}
+			</Card.Content>
+
+			<Card.Footer>
+				<Card.Text>
+					<div>Peak times</div>
+					<div>39min</div>
+				</Card.Text>
+			</Card.Footer>
+		</Card.Root>
+	{/each}
 </div>
