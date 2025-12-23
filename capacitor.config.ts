56ed2618854d71e17038b6db1a723d8aae7c6a2a
joshidoshi <joshua.dietiker@gmail.com>
2025-12-23 23:49:52 +0100

    revert capacitor config to normal

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'public-transport-app',
-  webDir: 'dist',
-  server: {
-    // This points the app to your Vite server immediately on launch
-    url: 'http://192.168.1.118:5173', 
-    cleartext: true,
-    androidScheme: 'http'
-  }
+  appId: 'com.thruut.transport',
+  appName: 'Transport',
+  webDir: 'dist'
 };
 
 export default config;
